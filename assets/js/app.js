d59a8f1fc98993d59ebd6b0161922003ac5cc1f5
Mel-Med <mel._.23@hotmail.com>
2022-10-28 19:45:27 -0500

    Se agregan cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,37 @@
+// Se corrigen los nombres comparado con html
+
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
 const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $b = document.querySelector('blog');
+const $l = document.querySelector('location');
 
-function displayUser(username) {
+  // Se usa una funcion Async por que existe un await
+async function displayUser(username) {
   $n.textContent = 'cargando...';
+  // Se agrega try, // se añade catch
+  try {
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
   $n.textContent = '${data.name}';
   $b.textContent = '${data.blog}';
   $l.textContent = '${data.location}';
+  
 }
+catch(e){
+  handleError(e)
+
+}
+}
+
 
 function handleError(err) {
   console.log('OH NO!');
-  console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  //Se cambia las comillas para imprimir
+  console.log('err');
+  $n.textContent = `Algo salió mal: ${err}`;
+
+  
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski').catch(handleError); 
